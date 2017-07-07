55ed494c208e612b69a2438090084869f580eaef
Sebastian Wilzbach <seb@wilzba.ch>
2017-07-07 17:19:50 +0200

    Allow passing in stdin and custom compiler arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ set -u
 set -o pipefail
 
 cd /sandbox
-echo "$*" | base64 -d > onlineapp.d
+echo "$1" | base64 -d > onlineapp.d
 
-exec timeout -s KILL ${TIMEOUT:-20} ${DLANG_EXEC} -run onlineapp.d | tail -n100
+args=${DOCKER_FLAGS:-""}
+if [ -z ${2:-""} ] ; then
+    exec timeout -s KILL ${TIMEOUT:-20} rdmd $args onlineapp.d | tail -n100
+else
+    exec timeout -s KILL ${TIMEOUT:-20} bash -c "echo $2 | base64 -d | rdmd $args onlineapp.d | tail -n100"
+fi
