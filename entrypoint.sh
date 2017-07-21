e2c83a3768ebc10d3802e9dbc3b12ef1c3083e42
Sebastian Wilzbach <seb@wilzba.ch>
2017-07-21 02:52:57 +0200

    First DUB support with hard-coded dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,10 @@ cd /sandbox
 echo "$1" | base64 -d > onlineapp.d
 
 args=${DOCKER_FLAGS:-""}
-if [ -z ${2:-""} ] ; then
-    exec timeout -s KILL ${TIMEOUT:-20} ${DLANG_EXEC} -run $args onlineapp.d | tail -n100
+if  grep -qE "dub[.](sdl|json):" onlineapp.d > /dev/null 2>&1  ; then
+    exec timeout -s KILL ${TIMEOUT:-20} dub -q --compiler=${DLANG_EXEC} --single --skip-registry=all onlineapp.d | tail -n100
+elif [ -z ${2:-""} ] ; then
+    exec timeout -s KILL ${TIMEOUT:-20} ${DLANG_EXEC} $args -run onlineapp.d | tail -n100
 else
-    exec timeout -s KILL ${TIMEOUT:-20} bash -c "echo $2 | base64 -d | ${DLANG_EXEC} -run $args onlineapp.d | tail -n100"
+    exec timeout -s KILL ${TIMEOUT:-20} bash -c "echo $2 | base64 -d | ${DLANG_EXEC} $args -run onlineapp.d | tail -n100"
 fi
