91bd675ce163dd7fa35cd423e30cce37865039f0
Sebastian Wilzbach <seb@wilzba.ch>
2018-01-26 12:21:22 +0100

    Use ddemangle for dmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,9 +13,11 @@ export TERM="dtour"
 compiler="${DLANG_EXEC}"
 return_asm=0
 return_file=
+ddemangle="cat"
 if [[ $args =~ .*-asm.* ]] ; then
     args="${args/-asm/-c}"
     return_asm=1
+    ddemangle="ddemangle"
 elif [[ $args =~ .*-D.* ]] ; then
     args="${args/-D/-D -c -o-}"
     return_file="onlineapp.html"
@@ -23,10 +25,12 @@ elif [[ $args =~ .*-output-s.* ]] ; then
     args="${args/-output-s/-output-s -c}"
     compiler=ldc2
     return_file="onlineapp.s"
+    ddemangle="ddemangle"
 elif [[ $args =~ .*-output-ll.* ]] ; then
     args="${args/-output-ll/-output-ll -c}"
     compiler=ldc2
     return_file="onlineapp.ll"
+    ddemangle="ddemangle"
 elif [[ $args =~ .*-vcg-ast.* ]] ; then
     args="${args/-vcg-ast/-vcg-ast -c -o-}"
     return_file="onlineapp.d.cg"
@@ -38,11 +42,11 @@ if  grep -qE "dub[.](sdl|json):" onlineapp.d > /dev/null 2>&1  ; then
     exec timeout -s KILL ${TIMEOUT:-30} dub -q --compiler=${DLANG_EXEC} --single --skip-registry=all onlineapp.d | tail -n10000
 elif [ $return_asm -eq 1 ] ; then
     exec timeout -s KILL ${TIMEOUT:-30} bash -c "${DLANG_EXEC} $args -g onlineapp.d | tail -n100; \
-        obj2asm onlineapp.o | tail -n10000;"
+        obj2asm onlineapp.o | $ddemangle | tail -n10000;"
 elif [[ $args =~ .*-c.* ]] ; then
     exec timeout -s KILL ${TIMEOUT:-30} bash -c "${compiler} $args onlineapp.d | tail -n100; \
     if [ -f $return_file ] ; then \
-        cat "$return_file" | tail -n10000; \
+        cat "$return_file" | $ddemangle | tail -n10000; \
     fi"
 elif [ -z ${2:-""} ] ; then
     exec timeout -s KILL ${TIMEOUT:-30} \
