8899f3545654e5c20daeeb5c0c2723a5ae4ac2e4
Sebastian Wilzbach <seb@wilzba.ch>
2018-02-20 04:02:36 +0100

    First har support

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ compiler="${DLANG_EXEC}"
 return_asm=0
 return_file=
 ddemangle="cat"
+onlineapp="onlineapp.d"
+
 if [[ $args =~ .*-asm.* ]] ; then
     args="${args/-asm/-c}"
     return_asm=1
@@ -38,23 +40,29 @@ elif [[ $args =~ .*-Xf=-* ]] ; then
     args="${args/-Xf=-/-Xf=- -c -o-}"
 elif [[ $args =~ .*-c.* ]] ; then
     args="${args/-c/-c -o-}"
+else
+    args="$args -run"
 fi
 
-if  grep -qE "dub[.](sdl|json):" onlineapp.d > /dev/null 2>&1  ; then
-    exec timeout -s KILL ${TIMEOUT:-30} dub -q --compiler=${DLANG_EXEC} --single --skip-registry=all onlineapp.d | tail -n10000
+if grep -q "^--- .*d" "$onlineapp" > /dev/null 2>&1  ; then
+    mv "$onlineapp" onlineapp.har
+    onlineapp=$(har "onlineapp.har" | grep "[.d]$" | paste -s -d ' ')
+    exec timeout -s KILL ${TIMEOUT:-30} bash -c "${DLANG_EXEC} -g $args $onlineapp | tail -n100000"
+elif  grep -qE "dub[.](sdl|json):" "$onlineapp" > /dev/null 2>&1  ; then
+    exec timeout -s KILL ${TIMEOUT:-30} dub -q --compiler=${DLANG_EXEC} --single --skip-registry=all "$onlineapp" | tail -n10000
 elif [ $return_asm -eq 1 ] ; then
-    exec timeout -s KILL ${TIMEOUT:-30} bash -c "${DLANG_EXEC} $args -g onlineapp.d | tail -n100; \
+    exec timeout -s KILL ${TIMEOUT:-30} bash -c "${DLANG_EXEC} $args -g "$onlineapp" | tail -n100; \
         obj2asm onlineapp.o | $ddemangle | tail -n500000;"
 elif [[ $args =~ .*-c.* ]] ; then
-    exec timeout -s KILL ${TIMEOUT:-30} bash -c "${compiler} $args onlineapp.d | tail -n100; \
+    exec timeout -s KILL ${TIMEOUT:-30} bash -c "${compiler} $args "$onlineapp" | tail -n100; \
     if [ -f $return_file ] ; then \
         cat "$return_file" | $ddemangle | tail -n500000; \
     fi"
 elif [ -z ${2:-""} ] ; then
     exec timeout -s KILL ${TIMEOUT:-30} \
-        bash -c 'faketty () { script -qfc "$(printf "%q " "$@")" /dev/null ; };'"faketty ${DLANG_EXEC} $args -color=$coloring -g -run onlineapp.d | cat" \
+        bash -c 'faketty () { script -qfc "$(printf "%q " "$@")" /dev/null ; };'"faketty ${DLANG_EXEC} -color=$coloring -g $args "$onlineapp" | cat" \
         | sed 's/\r//' \
         | tail -n10000
 else
-    exec timeout -s KILL ${TIMEOUT:-30} bash -c "echo $2 | base64 -d | ${DLANG_EXEC} $args -g -run onlineapp.d | tail -n10000"
+    exec timeout -s KILL ${TIMEOUT:-30} bash -c "echo $2 | base64 -d | ${DLANG_EXEC} -g $args "$onlineapp" | tail -n10000"
 fi
