0074d56817744c80b355934f574d110f738835df
MoonlightSentinel <moonlightsentinel@disroot.org>
2021-03-09 03:56:31 +0100

    Replace piped grep with custom function
    They apparently trigger errors with broken pipes and a custom function
    also allows for a better error reporting

--- test.sh
+++ test.sh
@@ -9,6 +9,27 @@ err_report() {
 }
 trap 'err_report $LINENO' ERR
 
+grepOutput() {
+    local output_file="command_output.tmp"
+
+    if ! "${@:1:(($# - 1))}" > $output_file
+    then
+        echo "Command failed!"
+
+    elif ! grep -q "${@:$#}" $output_file
+    then
+        echo "grep failed!"
+
+    else
+        rm $output_file
+        return 0
+    fi
+
+    cat $output_file
+    rm $output_file
+    exit 1
+}
+
 # simple hello world
 source='void main() { import std.stdio; writeln("Hello World"); }'
 bsource=$(echo $source | base64 -w0)
@@ -53,21 +74,21 @@ bsource=$(echo $source | base64 -w0)
 # Test -vcg-ast
 source='void main() { foreach (i; [1, 2]) {} }'
 bsource=$(echo $source | base64 -w0)
-DOCKER_FLAGS="-vcg-ast" docker run -e DOCKER_FLAGS --rm $dockerId $bsource | grep -q "__key"
+grepOutput docker run -e DOCKER_FLAGS="-vcg-ast" --rm $dockerId $bsource "__key"
 
 # Check -asm (DMD-only)
 if [[ ! $dockerId =~ "ldc" ]] ; then
     source='void main() { int a; }'
     bsource=$(echo $source | base64 -w0)
-    DOCKER_FLAGS="-asm" docker run -e DOCKER_FLAGS --rm $dockerId $bsource | grep -q "_Dmain"
+    grepOutput docker run -e DOCKER_FLAGS="-asm" --rm $dockerId $bsource "_Dmain"
 fi
 
 # Check -ouput-ll and -output-s
 if [[ $dockerId =~ "ldc" ]] ; then
     source='void main() { int a; }'
     bsource=$(echo $source | base64 -w0)
-    DOCKER_FLAGS="-output-ll" docker run -e DOCKER_FLAGS --rm $dockerId $bsource | grep -q "@ldc.register_dso"
-    DOCKER_FLAGS="-output-s" docker run -e DOCKER_FLAGS --rm $dockerId $bsource | grep -q "ldc.register_dso:"
+    grepOutput docker run -e DOCKER_FLAGS="-output-ll" --rm $dockerId $bsource "@ldc.register_dso"
+    grepOutput docker run -e DOCKER_FLAGS="-output-s" --rm $dockerId $bsource "ldc.register_dso:"
 fi
 
 # Check AddressSanitizer output with line numbers (LDC-only)
@@ -75,35 +96,35 @@ if [[ $dockerId =~ "ldc" ]] ; then
     source=$'void main() { \n int a; \n int* ap = &a + 1; \n *ap = 0; }'
     #                                                        ^ line 4 column 2
     bsource=$(echo "$source" | base64 -w0)
-    DOCKER_FLAGS="-fsanitize=address -g" docker run -e DOCKER_FLAGS --rm $dockerId $bsource | grep -q '#0 0x[0-9a-f]* in ..main [/a-z\.]*:4:2'
+    grepOutput docker run -e DOCKER_FLAGS="-fsanitize=address -g" --rm $dockerId $bsource '#0 0x[0-9a-f]* in ..main [/a-z\.]*:4:2'
 fi
 
 # Check HTML output
 source='///\nvoid main(){}'
 bsource=$(echo $source | base64 -w0)
-DOCKER_FLAGS="-D" docker run -e DOCKER_FLAGS --rm $dockerId $bsource | grep -q "<html>"
+grepOutput docker run -e DOCKER_FLAGS="-D" --rm $dockerId $bsource "<html>"
 
 # Check JSON output
 source='///\nvoid main(){}'
 bsource=$(echo $source | base64 -w0)
-DOCKER_FLAGS="-Xf=-" docker run -e DOCKER_FLAGS --rm $dockerId $bsource | grep -q '"file" : "onlineapp.d"'
+grepOutput docker run -e DOCKER_FLAGS="-Xf=-" --rm $dockerId $bsource '"file" : "onlineapp.d"'
 
 # Check Har
 source="--- test.d\nvoid main(){import std.stdio; __FILE__.writeln;}"
 bsource=$(echo -e "$source" | base64 -w0)
-docker run --rm $dockerId $bsource | grep -q "test.d"
+grepOutput docker run --rm $dockerId $bsource "test.d"
 
 # Check har with multiple files
 source="--- test.d\nvoid main(){import bar; foo();}\n--- bar.d\nvoid foo(){import std.stdio; __FILE__.writeln;}"
 bsource=$(echo -e "$source" | base64 -w0)
-docker run --rm $dockerId $bsource | grep -q "bar.d"
+grepOutput docker run --rm $dockerId $bsource "bar.d"
 
 # Har - minimal runtime
 # Only works on dmd-nightly for now
 if [[ $1 = *nightly ]] ; then
 source="--- object.d\nmodule object;\n--- bar.d\nextern(C) void main(){printf(\"%s\", __MODULE__.ptr);\n}"
 bsource=$(echo -e "$source" | base64 -w0)
-DOCKER_FLAGS="-conf= -defaultlib=" docker run -e DOCKER_FLAGS --rm $dockerId $bsource 2>&1 | grep -q '`printf` is not defined'
+grepOutput docker run -e DOCKER_FLAGS="-conf= -defaultlib=" --rm $dockerId $bsource '`printf` is not defined'
 fi
 
 # Check dpp Hello World
